2e3a4c864e20f25cb6f3d7ecbd916ca00dea580a
Zacck Osiemo <coderv63@gmail.com>
2025-09-18 17:03:38 +0200

    Add Immediate Type Handling
    - Move to 32 bit instruction registers
    - Add wires to access IR sub components
    - Handle Itype and RType in opcodes

--- main.v
+++ main.v
@@ -16,7 +16,23 @@ module tiny_cpu (
   //Program Counter 
   reg [3:0] PC; 
   //ROM Memory 16 registers whose size is 8 bits
-  reg [7:0] ROM [0:15];
+  reg [31:0] ROM [0:15];
+
+  /* Wires to access registers */
+  reg [31:0] IR; 
+
+  wire [6:0] opcode = IR[6:0];
+  wire [4:0] rd     = IR[11:7]; 
+  wire [3:0] funct3 = IR[14:12];
+  wire [4:0] rs1    = IR[19:15]; 
+  wire [4:0] rs2    = IR[24:20];
+  wire [6:0] funct7 = IR[31:25];
+
+  wire signed [31:0] imm_i = {{20{IR[31]}}, IR[31:20]};
+
+  /*hold computation for LED*/
+  reg [4:0] last_written_reg; 
+
 
   /*Run the FPGA oscillator*/
   always @(posedge CLK) begin
@@ -31,48 +47,61 @@ module tiny_cpu (
   *  The lower 4 bits now become the register to use
    */
   initial begin
-	  ROM[0] = 8'b0001_0000; // go to next instruction
-	  ROM[1] = 8'b0010_0000; // Output result of computation
-	  ROM[2] = 8'b0001_0001; 
-	  ROM[3] = 8'b0010_0001;
- 	  ROM[4] = 8'b0001_0010;  
-	  ROM[5] = 8'b0010_0010;  
-	  ROM[6] = 8'b0001_0011;  
-	  ROM[7] = 8'b0010_0011;  
-	  ROM[8] = 8'b0001_0100;  
-	  ROM[9] = 8'b0010_0100;
-	  ROM[10] = 8'b0001_0101; 
-	  ROM[11] = 8'b0010_0101;
- 	  ROM[12] = 8'b0001_0110;  
-	  ROM[13] = 8'b0010_0110;  
-	  ROM[14] = 8'b0001_0111;  
-	  ROM[15] = 8'b0010_0111;
+	  ROM[0] = 32'h00500093;  // ADDI x1, x0, 5 
+	  ROM[1] = 32'h00300113; // ADD1 x1, x2, 3 
+	  ROM[2] = 32'h002081B3; // ADD x3, x1, 2
+	  ROM[3] = 32'h0071F213; // ANDI x4, x3,7
+ 	  ROM[4] = 32'h0011E293; // ORI  x5, x3, 1
+	  ROM[5] = 32'h00000000;  //NOPS
+	  ROM[6] = 32'h00000000;  
+	  ROM[7] = 32'h00000000;  
+	  ROM[8] = 32'h00000000;  
+	  ROM[9] = 32'h00000000;
+	  ROM[10] = 32'h00000000; 
+	  ROM[11] = 32'h00000000;
+ 	  ROM[12] = 32'h00000000;  
+	  ROM[13] = 32'h00000000;  
+	  ROM[14] = 32'h00000000;  
+	  ROM[15] = 32'h00000000;
   end
   
-  /*
-   *  The instructions upper 4 bits are the opcode 
-   *  0001 INC , 0010  ADD, 0011 SUB, 0100 AND, 0101 OR, 
-   *  0110 XOR , 0111 SHL, 1000 SHR
-   */ 
+  /* Actually run ourt CPU */
   always @(posedge slow_clk) begin
-  	case(ROM[PC][7:4])
-	  4'b0001: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] + 1;	//INC 
-	  4'b0010: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] + R[0];   //ADD
-	  4'b0011: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] - R[0];   //SUB
-	  4'b0100: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] & R[1];   //AND
-	  4'b0101: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] | R[1];   //OR
-	  4'b0110: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] ^ R[1];   //XOR
-	  4'b0111: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] << 1;     //SHL
-	  4'b1000: R[ROM[PC][3:0]] <= R[ROM[PC][3:0]] >> 1;     //SHR
-	  default: ;
-	endcase
+	  IR <= ROM[PC]; 
+
+	  case(opcode)
+		  //RTYPE
+		  7'b0110011: begin
+			  case(funct3)
+				  3'b000: begin 
+				  if(funct7 == 7'b0100000) R[rd] <= R[rs1] - R[rs2]; // SUB
+				  else 			   R[rd] <= R[rs1] + R[rs2]; // ADD
+			  end
+		  	  3'b111: R[rd] <= R[rs1] & R[rs2]; //AND 
+			  3'b110: R[rd] <= R[rs1] | R[rs2]; //OR
+			  3'b100: R[rd] <= R[rs1] ^ R[rs2]; //XOR
+			  default: ;
+		  endcase
+		  last_written_reg <= rd;
+	  end
+	  	7'b0010011: begin
+		case (funct3)
+			3'b000: R[rd] <= R[rs1] + imm_i; //ADDI
+			3'b111: R[rd] <= R[rs1] & imm_i; //ANDI
+			3'b110: R[rd] <= R[rs1] | imm_i; //ORI
+			default: ;
+		endcase
+		last_written_reg <= rd; 
+		end 
+		default: ;
+	endcase 
 	PC <= PC + 1;
 	/* R0 is always a 0*/
 	R[0] <= 32'b0; 
   end
 
-  assign led_blue = 	R[ROM[PC][3:0]][2]; 
-  assign led_green =	R[ROM[PC][3:0]][1]; 
-  assign led_red = 	R[ROM[PC][3:0]][0];
+  assign led_blue = 	R[last_written_reg][2]; 
+  assign led_green =	R[last_written_reg][1]; 
+  assign led_red = 	R[last_written_reg][0];
 
 endmodule
